778f0820acdc4664fb129ad5d5ca6a589b0c9e84
michaelafdez <michaelafdez@yahoo.com>
2020-03-23 12:36:29 -0400

    Hand Easing and Spotlight
    Eased hand movement, added a spotlight underneath so player can tell where hand is

--- SpacetimeLiaison_v2/Assets/Scripts/playerMovement/handScript.cs
+++ SpacetimeLiaison_v2/Assets/Scripts/playerMovement/handScript.cs
@@ -10,7 +10,15 @@ public class handScript : MonoBehaviour
     public float mouseX, mouseY;
     public string mouseXInput, mouseYInput;
 
-    public float sensitivity;
+    public float sensitivity, smoothing;
+
+    Vector3 smoother;
+
+    private RaycastHit hit;
+
+    int layerMask = 1 << 8;
+
+    float rayDistance = 5;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +27,28 @@ public class handScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
+    {
+        HandMovement();
+
+        Debug.DrawRay(transform.position, -transform.up, Color.magenta);
+
+        if (Physics.Raycast(transform.position, -transform.up, out hit, rayDistance, layerMask))
+        {
+            Debug.Log("hit");
+        }
+    }
+
+    //moves hand according to mouse movement
+    void HandMovement()
     {
         mouseX = Input.GetAxis(mouseXInput) * sensitivity * Time.deltaTime;
         mouseY = Input.GetAxis(mouseYInput) * sensitivity * Time.deltaTime;
 
-        thisRigid.velocity = new Vector3(mouseX, thisRigid.velocity.y, mouseY);
+        smoother.x = Mathf.Lerp(smoother.x, mouseX, 1f / smoothing);
+        smoother.z = Mathf.Lerp(smoother.z, mouseY, 1f / smoothing);
+        smoother.y = thisRigid.velocity.y;
+
+        thisRigid.velocity = smoother;
     }
 }
