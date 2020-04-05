f7cbcad88bf83f7c2024a726ffec36c3dfb3f473
michaelafdez <michaelafdez@yahoo.com>
2020-04-05 19:08:08 -0400

    New Basic Hand Movement
    Hand can now move using the new input system

--- SpacetimeLiaison_v2/Assets/Scripts/playerMovement/InputMaster.cs
+++ SpacetimeLiaison_v2/Assets/Scripts/playerMovement/InputMaster.cs
@@ -15,7 +15,7 @@ public class @InputMaster : IInputActionCollection, IDisposable
     ""name"": ""InputMaster"",
     ""maps"": [
         {
-            ""name"": ""Player"",
+            ""name"": ""TestPlayer"",
             ""id"": ""86e68bde-397b-43aa-aef9-6c93411baef6"",
             ""actions"": [
                 {
@@ -204,6 +204,208 @@ public class @InputMaster : IInputActionCollection, IDisposable
                     ""isPartOfComposite"": true
                 }
             ]
+        },
+        {
+            ""name"": ""Player"",
+            ""id"": ""b5d3a583-8e56-498d-b4e8-44fb408d07e2"",
+            ""actions"": [
+                {
+                    ""name"": ""Move"",
+                    ""type"": ""Value"",
+                    ""id"": ""7d6f02fd-a7d7-4113-88c1-de79caf5fdab"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Choices"",
+                    ""type"": ""Button"",
+                    ""id"": ""6f364760-1666-4f3e-bd2c-8b3cb46954fb"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Grab"",
+                    ""type"": ""Button"",
+                    ""id"": ""0db692c0-1fa7-41ff-93d7-994d938653e3"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Interact"",
+                    ""type"": ""Button"",
+                    ""id"": ""8986fd3e-6d46-4ae2-8149-f8f29e0547b7"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Rotate"",
+                    ""type"": ""Button"",
+                    ""id"": ""2194e941-1b12-4c03-99fd-752476bf56d4"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""9d4b6f9a-db65-4db9-b796-da181f578b0d"",
+                    ""path"": ""<XInputController>/buttonWest"",
+                    ""interactions"": ""Press"",
+                    ""processors"": """",
+                    ""groups"": ""Controller"",
+                    ""action"": ""Choices"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""e92bd8fb-d86a-4ad9-89e6-3518e8dd627f"",
+                    ""path"": ""<XInputController>/buttonEast"",
+                    ""interactions"": ""Press"",
+                    ""processors"": """",
+                    ""groups"": ""Controller"",
+                    ""action"": ""Choices"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""01f6206a-b01c-4473-a607-0166a3bd9e98"",
+                    ""path"": ""<XInputController>/buttonNorth"",
+                    ""interactions"": ""Press"",
+                    ""processors"": """",
+                    ""groups"": ""Controller"",
+                    ""action"": ""Choices"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""309967c1-a564-4f61-acb7-e12d17e6ae9d"",
+                    ""path"": ""<XInputController>/buttonSouth"",
+                    ""interactions"": ""Press"",
+                    ""processors"": """",
+                    ""groups"": ""Controller"",
+                    ""action"": ""Choices"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""d49dd044-9f1e-4dd0-a50a-0eefec117903"",
+                    ""path"": ""<XInputController>/leftTrigger"",
+                    ""interactions"": ""Hold"",
+                    ""processors"": """",
+                    ""groups"": ""Controller"",
+                    ""action"": ""Grab"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""438837be-8402-4e3a-a32f-5f32ad5b3e0c"",
+                    ""path"": ""<XInputController>/rightTrigger"",
+                    ""interactions"": ""Press"",
+                    ""processors"": """",
+                    ""groups"": ""Controller"",
+                    ""action"": ""Interact"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""4227a4b7-ec2b-4762-8759-1aa6683ef452"",
+                    ""path"": ""<XInputController>/leftShoulder"",
+                    ""interactions"": ""Hold"",
+                    ""processors"": ""InvertVector2(invertY=false)"",
+                    ""groups"": ""Controller"",
+                    ""action"": ""Rotate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""e65e965c-eaef-446a-a343-3098cdef6774"",
+                    ""path"": ""<XInputController>/rightShoulder"",
+                    ""interactions"": ""Hold"",
+                    ""processors"": """",
+                    ""groups"": ""Controller"",
+                    ""action"": ""Rotate"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""ControllerMove"",
+                    ""id"": ""f7fc709d-8bfb-4b3e-8454-90dba387378b"",
+                    ""path"": ""2DVector(mode=2)"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Move"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""9ee518c3-3880-4817-8106-94d1bde2a78b"",
+                    ""path"": ""<XInputController>/leftStick/up"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Controller"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""1530db3a-968d-47e9-96cb-3c08fe7a3c49"",
+                    ""path"": ""<XInputController>/leftStick/down"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Controller"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""bfd24f63-1da6-4ee0-b31e-57aa14e42fdd"",
+                    ""path"": ""<XInputController>/leftStick/left"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Controller"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""e34b89ed-9029-4878-8d45-c88afbfd1b45"",
+                    ""path"": ""<XInputController>/leftStick/right"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Controller"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""e8050b08-db95-48c4-8c08-a53a01cf67fb"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Keyboard"",
+                    ""action"": ""Move"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
         }
     ],
     ""controlSchemes"": [
@@ -227,9 +429,21 @@ public class @InputMaster : IInputActionCollection, IDisposable
                     ""isOR"": false
                 }
             ]
+        },
+        {
+            ""name"": ""Keyboard"",
+            ""bindingGroup"": ""Keyboard"",
+            ""devices"": []
         }
     ]
 }");
+        // TestPlayer
+        m_TestPlayer = asset.FindActionMap("TestPlayer", throwIfNotFound: true);
+        m_TestPlayer_Move = m_TestPlayer.FindAction("Move", throwIfNotFound: true);
+        m_TestPlayer_Choices = m_TestPlayer.FindAction("Choices", throwIfNotFound: true);
+        m_TestPlayer_Grab = m_TestPlayer.FindAction("Grab", throwIfNotFound: true);
+        m_TestPlayer_Interact = m_TestPlayer.FindAction("Interact", throwIfNotFound: true);
+        m_TestPlayer_Rotate = m_TestPlayer.FindAction("Rotate", throwIfNotFound: true);
         // Player
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
@@ -282,6 +496,71 @@ public class @InputMaster : IInputActionCollection, IDisposable
     {
         asset.Disable();
     }
+
+    // TestPlayer
+    private readonly InputActionMap m_TestPlayer;
+    private ITestPlayerActions m_TestPlayerActionsCallbackInterface;
+    private readonly InputAction m_TestPlayer_Move;
+    private readonly InputAction m_TestPlayer_Choices;
+    private readonly InputAction m_TestPlayer_Grab;
+    private readonly InputAction m_TestPlayer_Interact;
+    private readonly InputAction m_TestPlayer_Rotate;
+    public struct TestPlayerActions
+    {
+        private @InputMaster m_Wrapper;
+        public TestPlayerActions(@InputMaster wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Move => m_Wrapper.m_TestPlayer_Move;
+        public InputAction @Choices => m_Wrapper.m_TestPlayer_Choices;
+        public InputAction @Grab => m_Wrapper.m_TestPlayer_Grab;
+        public InputAction @Interact => m_Wrapper.m_TestPlayer_Interact;
+        public InputAction @Rotate => m_Wrapper.m_TestPlayer_Rotate;
+        public InputActionMap Get() { return m_Wrapper.m_TestPlayer; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(TestPlayerActions set) { return set.Get(); }
+        public void SetCallbacks(ITestPlayerActions instance)
+        {
+            if (m_Wrapper.m_TestPlayerActionsCallbackInterface != null)
+            {
+                @Move.started -= m_Wrapper.m_TestPlayerActionsCallbackInterface.OnMove;
+                @Move.performed -= m_Wrapper.m_TestPlayerActionsCallbackInterface.OnMove;
+                @Move.canceled -= m_Wrapper.m_TestPlayerActionsCallbackInterface.OnMove;
+                @Choices.started -= m_Wrapper.m_TestPlayerActionsCallbackInterface.OnChoices;
+                @Choices.performed -= m_Wrapper.m_TestPlayerActionsCallbackInterface.OnChoices;
+                @Choices.canceled -= m_Wrapper.m_TestPlayerActionsCallbackInterface.OnChoices;
+                @Grab.started -= m_Wrapper.m_TestPlayerActionsCallbackInterface.OnGrab;
+                @Grab.performed -= m_Wrapper.m_TestPlayerActionsCallbackInterface.OnGrab;
+                @Grab.canceled -= m_Wrapper.m_TestPlayerActionsCallbackInterface.OnGrab;
+                @Interact.started -= m_Wrapper.m_TestPlayerActionsCallbackInterface.OnInteract;
+                @Interact.performed -= m_Wrapper.m_TestPlayerActionsCallbackInterface.OnInteract;
+                @Interact.canceled -= m_Wrapper.m_TestPlayerActionsCallbackInterface.OnInteract;
+                @Rotate.started -= m_Wrapper.m_TestPlayerActionsCallbackInterface.OnRotate;
+                @Rotate.performed -= m_Wrapper.m_TestPlayerActionsCallbackInterface.OnRotate;
+                @Rotate.canceled -= m_Wrapper.m_TestPlayerActionsCallbackInterface.OnRotate;
+            }
+            m_Wrapper.m_TestPlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Move.started += instance.OnMove;
+                @Move.performed += instance.OnMove;
+                @Move.canceled += instance.OnMove;
+                @Choices.started += instance.OnChoices;
+                @Choices.performed += instance.OnChoices;
+                @Choices.canceled += instance.OnChoices;
+                @Grab.started += instance.OnGrab;
+                @Grab.performed += instance.OnGrab;
+                @Grab.canceled += instance.OnGrab;
+                @Interact.started += instance.OnInteract;
+                @Interact.performed += instance.OnInteract;
+                @Interact.canceled += instance.OnInteract;
+                @Rotate.started += instance.OnRotate;
+                @Rotate.performed += instance.OnRotate;
+                @Rotate.canceled += instance.OnRotate;
+            }
+        }
+    }
+    public TestPlayerActions @TestPlayer => new TestPlayerActions(this);
 
     // Player
     private readonly InputActionMap m_Player;
@@ -355,6 +634,23 @@ public class @InputMaster : IInputActionCollection, IDisposable
             if (m_ControllerSchemeIndex == -1) m_ControllerSchemeIndex = asset.FindControlSchemeIndex("Controller");
             return asset.controlSchemes[m_ControllerSchemeIndex];
         }
+    }
+    private int m_KeyboardSchemeIndex = -1;
+    public InputControlScheme KeyboardScheme
+    {
+        get
+        {
+            if (m_KeyboardSchemeIndex == -1) m_KeyboardSchemeIndex = asset.FindControlSchemeIndex("Keyboard");
+            return asset.controlSchemes[m_KeyboardSchemeIndex];
+        }
+    }
+    public interface ITestPlayerActions
+    {
+        void OnMove(InputAction.CallbackContext context);
+        void OnChoices(InputAction.CallbackContext context);
+        void OnGrab(InputAction.CallbackContext context);
+        void OnInteract(InputAction.CallbackContext context);
+        void OnRotate(InputAction.CallbackContext context);
     }
     public interface IPlayerActions
     {
--- SpacetimeLiaison_v2/Assets/Scripts/playerMovement/playerActions.cs
+++ SpacetimeLiaison_v2/Assets/Scripts/playerMovement/playerActions.cs
@@ -7,17 +7,46 @@ public class playerActions : MonoBehaviour
 {
     InputMaster inputAction;
 
-    Vector2 moveInput;
+    Vector2 moveInput, smoothMouse;
+
+    public Vector2 moveDelta;
+
+    public float sensitivity, smoothing, maxSpeed;
+
+    private Rigidbody thisRigid;
 
     private void Awake()
     {
         inputAction = new InputMaster();
         inputAction.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
     }
+
+    private void Start()
+    {
+        thisRigid = gameObject.GetComponent<Rigidbody>();
+    }
+
     private void FixedUpdate()
     {
-        float h = moveInput.x;
-        float v = moveInput.y;
+        //float h = moveInput.x;
+        //float v = moveInput.y;
+
+        thisRigid.velocity = Vector3.ClampMagnitude(thisRigid.velocity, maxSpeed);
+    }
+
+    private void Update()
+    {
+        moveDelta = inputAction.Player.Move.ReadValue<Vector2>();
+
+        moveDelta = Vector2.Scale(moveDelta, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+
+        smoothMouse.x = Mathf.Lerp(smoothMouse.x, moveDelta.x, 1f / smoothing);
+        smoothMouse.y = Mathf.Lerp(smoothMouse.y, moveDelta.y, 1f / smoothing);
+
+        //thisRigid.velocity = new Vector3(moveDelta.x * sensitivity, thisRigid.velocity.y, moveDelta.y * sensitivity);
+        thisRigid.velocity = new Vector3(smoothMouse.x * sensitivity, thisRigid.velocity.y, smoothMouse.y * sensitivity);
+
+        //Debug.Log(thisRigid.velocity.magnitude);
     }
 
     private void OnEnable()
